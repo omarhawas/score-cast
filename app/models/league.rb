a2b61ac192794feca761f55476aee261849b4e5a
Omar Hawas <omarhawas94@gmail.com>
2024-06-13 18:45:06 -0400

    added leagueUsers to a league

--- app/models/league.rb
+++ app/models/league.rb
@@ -2,6 +2,8 @@ class League < ApplicationRecord
   belongs_to :admin_user, class_name: 'User'
   belongs_to :winner_user, class_name: 'User', optional: true
   belongs_to :tournament
+  has_many :league_users
+  has_many :users, through: :league_users 
   
   validates :name, presence: true
   validates :tournament_id, presence: true
