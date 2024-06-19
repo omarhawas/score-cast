129a7a2ae0f9394ea2a73360cfd45e34c0500952
Omar Hawas <omarhawas@Omars-MacBook-Pro.local>
2024-06-19 01:01:28 -0400

    adding styling to all pages. fixed some game logic bugs

--- app/models/league.rb
+++ app/models/league.rb
@@ -2,7 +2,7 @@ class League < ApplicationRecord
   belongs_to :admin_user, class_name: 'User'
   belongs_to :winner_user, class_name: 'User', optional: true
   belongs_to :tournament
-  has_many :league_users
+  has_many :league_users, dependent: :destroy
   has_many :users, through: :league_users 
   
   validates :name, presence: true
