31025c62414008623e7d4a0b26a48e9cfbc11d5d
Omar Hawas <omarhawas@Omars-MacBook-Pro.local>
2024-06-20 22:18:40 -0400

    created new user show page to show all of a users predictions and points gained for each game

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,55 +1,3 @@
-# class UsersController < ApplicationController
-    # before_action :set_user, only: [:show, :edit, :update, :destroy]
-  
-  #   def index
-  #     @users = User.all
-  #   end
-  
-  #   def show
-  #     # Fetches a single user
-  #   end
-  
-  #   def new
-  #     @user = User.new
-  #   end
-  
-  #   def create
-  #     @user = User.new(user_params)
-  
-  #     if @user.save
-  #       redirect_to @user, notice: 'User was successfully created.'
-  #     else
-  #       render :new
-  #     end
-  #   end
-  
-  #   def edit
-  #     # Fetches a user for editing
-  #   end
-  
-  #   def update
-  #     if @user.update(user_params)
-  #       redirect_to @user, notice: 'User was successfully updated.'
-  #     else
-  #       render :edit
-  #     end
-  #   end
-  
-  #   def destroy
-  #     @user.destroy
-  #     redirect_to users_url, notice: 'User was successfully destroyed.'
-  #   end
-  
-  #   private
-  #     def set_user
-  #       @user = User.find(params[:id])
-  #     end
-  
-  #     def user_params
-  #       params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  #     end
-  # end
-
   class UsersController < ApplicationController
 
     before_action :require_signin, except: [:new, :create]
@@ -76,6 +24,7 @@
 
     def show
         @user = User.find(params[:id])
+        @game_predictions = @user.game_predictions.includes(:game)
     end
 
     def edit
