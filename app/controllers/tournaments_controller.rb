f739d0cbd6e70ecebdd883f4806d4504f4c17baf
Omar Hawas <omarhawas94@gmail.com>
2024-07-02 18:17:57 -0400

    added predicted scores and actual scores on tournament show page

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -8,6 +8,8 @@ class TournamentsController < ApplicationController
         @tournament = Tournament.find(params[:id])
         @leagues = @tournament.leagues.order(created_at: :desc).paginate(page: params[:leagues_page], per_page: 7)
         @games = @tournament.games.order(created_at: :desc).paginate(page: params[:games_page], per_page: 7)
+        @user = current_user
+        @game_predictions = @user.game_predictions.where(game: @games) if @user
     end
 
     def edit
